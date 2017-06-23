61370db42aebbaf8ee87d68c6799584f936d7803
jgzhu <251475337@qq.com>
2017-06-23 17:12:57 +0800

    初始化

---
+++ GPUImageTest/GPUImageTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPUImageTest
+//
+//  Created by 江贵铸 on 2017/6/23.
+//  Copyright © 2017年 江贵铸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
